bf19adecf4689f4c2fe5c699ffd0353e83481afa
hanyohades <hanyohades0@gmail.com>
2026-02-14 14:46:18 -0800

    add bottom up logic in buildMaxHeap

--- HeapBuilder.java
+++ HeapBuilder.java
@@ -1,6 +1,7 @@
 public class HeapBuilder {
 
     public static void main(String[] args) {
+        //Initialize array of WordFreq objects
         WordFreq[] arr = {
             new WordFreq("happy", 400),
             new WordFreq("satisfied", 100),
@@ -18,7 +19,17 @@ public class HeapBuilder {
     }
 
     public static void buildMaxHeap(WordFreq[] arr) {
+        int n = arr.length;
 
+        //loop run from the last non-leaf node, upward to the root (bottom up)
+        for (int i = n/2 - 1; i >= 0; i--) {
+            maxHeapify(arr, n, i);
+        }
+
+        //Print heapified array
+        for (WordFreq wf : arr) {
+            System.out.println(wf.word + " - " + wf.frequency);
+        }
     }
 
     public static void maxHeapify(WordFreq[] arr, int n, int i) {
