7dd4b61d3b46ee32cedc9db8215c9fdbb05683b8
hanyohades <hanyohades0@gmail.com>
2026-02-14 14:23:07 -0800

    added WordFreq class and HeapBuilder skeleton

---
+++ HeapBuilder.java
@@ -0,0 +1,28 @@
+public class HeapBuilder {
+
+    public static void main(String[] args) {
+        WordFreq[] arr = {
+            new WordFreq("happy", 400),
+            new WordFreq("satisfied", 100),
+            new WordFreq("neutral", 300),
+            new WordFreq("would buy again", 200),
+            new WordFreq("terrible", 160),
+            new WordFreq("inconvenient", 900),
+            new WordFreq("difficult to use", 100),
+            new WordFreq("easy to use", 140),
+            new WordFreq("would recommend to friends", 800),
+            new WordFreq("visit the store", 700),
+        };
+
+        buildMaxHeap(arr);
+    }
+
+    public static void buildMaxHeap(WordFreq[] arr) {
+
+    }
+
+    public static void maxHeapify(WordFreq[] arr, int n, int i) {
+        
+    }
+}
+
---
+++ WordFreq.java
@@ -0,0 +1,9 @@
+public class WordFreq {
+    String word;
+    int frequency;
+
+    public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
